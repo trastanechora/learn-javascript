78dae4ffcb15acf3e27eea23d3b72985a5e80c69
trastanechora <trastanechora@gmail.com>
2020-04-23 08:21:41 +0700

    Updated string explaination

--- 3. Running JavaScript Program/1. Variable & Data Types/2_string.js
+++ 3. Running JavaScript Program/1. Variable & Data Types/2_string.js
@@ -25,3 +25,31 @@ console.log(thirdCharacter); // will log 'r'
 // (!) Beginer tips
 // If you are not sure about which of `var`, `let`, and `const` is necessary to be use, then you
 // can use only `let`, but later you will learn by doing about their differences and it's importance
+
+// ======================================================================
+
+// Substring Method
+let originalString = "001Turing";
+// our first task it to 'grab' the first 3 characters of this string and put it in one string:
+let firstThree = originalString.substring(0, 3);
+// verify
+console.log("first three characters:", firstThree);
+// next, we will 'grab' every character except the first three in three different ways:
+// grab from before 4th character onward
+let restOfString_1 = originalString.substring(3);
+// grab from before 4th character to just before 9th character
+let restOfString_2 = originalString.substring(3, 9);
+// grab from before 4th character to just before character located at length of string (9th)
+let restOfString_3 = originalString.substring(3, originalString.length);
+// verify
+console.log("restOfString_1:", restOfString_1);
+console.log("restOfString_2:", restOfString_2);
+console.log("restOfString_3:", restOfString_3);
+
+// Parse a Number from a String
+let title = "001Turing";
+let firstThree = title.substring(0, 3);
+
+let parsedNumber = parseInt(firstThree);
+console.log("type of parsedNumber:", typeof parsedNumber);
+console.log("value of parsedNumber:", parsedNumber);
